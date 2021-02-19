7090d4e84959cedd1c4828f6835bce3457c54910
trung1102 <letsmile95@gmail.com>
2021-02-19 22:33:47 +0700

    creart part2.py

---
+++ howkteam/python_basic/2.string/part2.py
@@ -0,0 +1,19 @@
+# chuỗi trần
+# tránh lỗi khi sử dụng escape sequence
+
+# print(r'chuỗi này dễ bị \nhầm với escape sequence \t')
+
+
+# index chuỗi
+str_a = "Python"
+str_b = str_a[2] # "t"
+str_c = str_a[-1] # "n" là phần tử cuối cùng của chuỗi
+str_d = str_a[:2] # "Py"
+str_e = str_a[1:-3]
+str_f = str_a[3:1:-1] # lấy từ phải qua trái
+print(str_d)
+print(str_e)
+print(str_a[0::-1])
+
+
+#in
